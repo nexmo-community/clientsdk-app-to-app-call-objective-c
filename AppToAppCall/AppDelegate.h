da61726ed2f1875d8b4444a5141ad8aa8c855bfe
Abdul Ajetunmobi <abdul.ajetunmobi@vonage.com>
2020-08-25 12:18:28 +0100

    Initial Commit

---
+++ AppToAppCall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppToAppCall
+//
+//  Created by Abdulhakim Ajetunmobi on 25/08/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AppToAppCall/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AppToAppCall
+//
+//  Created by Abdulhakim Ajetunmobi on 25/08/2020.
+//  Copyright © 2020 Vonage. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
